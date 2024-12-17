daa056e887814c7a382729d32c1bb5e0ae85c415
Abcia2 <gianluca.arobaleno@gmail.com>
2024-12-17 12:45:33 +0100

    Aggiungere i file di progetto.

---
+++ Sistema di Allarme con Soglia di Sicurezza/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_di_Allarme_con_Soglia_di_Sicurezza
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
